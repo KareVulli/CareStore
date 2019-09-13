bbe1776bf01d5542be83c41e8339348c387af80c
Marvin Helstein <m_helstein@hotmail.com>
2019-09-13 09:59:08 +0300

    Create content elements in product page

---
+++ public/assets/js/product.js
@@ -0,0 +1,30 @@
+
+
+window.onload = () => {
+    const params = new URLSearchParams(window.location.search)
+    //alert('Toode: ' + params.get('title') + '\nHind: ' + params.get('price') + '\nPildi url: ' + params.get('image'))
+    
+    const container = document.getElementById('product');
+
+    const titleElement = document.createElement('p');
+    titleElement.textContent = params.get('title');
+    titleElement.classList.add('product-title');
+
+    const imageElement = document.createElement('img');
+    imageElement.src = params.get('image');
+    imageElement.classList.add('product-image');
+
+    const descriptionElement = document.createElement('p');
+    descriptionElement.textContent = params.get('description');
+    descriptionElement.classList.add('product-description');
+
+    const priceElement = document.createElement('p');
+    priceElement.textContent = params.get('price') + ' €';
+    priceElement.classList.add('product-price');
+
+    container.append(titleElement);
+    container.append(imageElement);
+    container.append(descriptionElement);
+    container.append(priceElement);
+
+}
