4c289f50b97c4ae4bd765b49a3315c688cff512f
Marvin Helstein <m_helstein@hotmail.com>
2019-10-24 01:05:20 +0300

    Fixed GET /users/by-email not working

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -9,9 +9,9 @@ router.get('/users', (req, res) => UsersController.getAll(req, res));
 router.post('/users', validation(userSchemas.user), (req, res) => UsersController.create(req, res));
 router.delete('/users', (req, res) => UsersController.deleteAll(req, res));
 
+router.get('/users/by-email', (req, res) => UsersController.isNewEmail(req, res));
+
 router.get('/users/:id', (req, res) => UsersController.get(req, res));
 router.delete('/users/:id', (req, res) => UsersController.delete(req, res));
-
-router.get('/users/by-email', (req, res) => UsersController.isNewEmail(req, res));
 
 export default router;
